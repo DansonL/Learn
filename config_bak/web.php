a3eaa57819f389e61373e6e544d4ef8906da9f70
Danson <594360516@qq.com>
2017-12-21 09:57:41 +0800

    url美化

--- config_bak/web.php
+++ config_bak/web.php
@@ -12,6 +12,12 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'FX44H5B8VJociMLYn69uD2hopZFUwjRC',
---
+++ controllers/RecordController.php
@@ -0,0 +1,15 @@
+<?php
+use yii\web\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/21 0021
+ * Time: 上午 09:55
+ */
+
+class RecordController extends Controller{
+    public function actionHello(){
+        echo 'hello';
+    }
+}
