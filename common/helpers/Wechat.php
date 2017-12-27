495da1c916ea9554d821bb1f9977d77be001e82d
Danson <594360516@qq.com>
2017-12-27 16:26:12 +0800

    默认回复

--- common/helpers/Wechat.php
+++ common/helpers/Wechat.php
@@ -134,6 +134,7 @@ class Wechat{
 
     public function _msgText($to, $from, $content) {
         $res = sprintf($this->_msg_template['text'], $to, $from, time(), $content);
+        Yii::trace($res);
         exit($res);
     }
 
